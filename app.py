614ed968c8c08e1579af7221fd8f9356dd89bcf5
Adam Martindale <carvingcravings@gmail.com>
2022-04-27 11:54:57 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, jsonify
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+
+Bootstrap(app)
+
+# website version
+
+
+@app.route('/')
+@app.route('/<username>')
+def home_page(username='World!'):
+    return render_template('home.html', username=username)
+
+# the json microservice version
+
+
+@app.route('/api/hello/')
+@app.route('/api/hello/<username>')
+def api(username='World!'):
+    return jsonify({'Hello': username})
